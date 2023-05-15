adb2d4e4bcdd20968f4f9d39e8b7444e1e75189e
Vibbing <nithinraju242@gmail.com>
2023-05-15 16:03:50 +0530

    [fix] => views location update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const dataBase = require('./database/connection')
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts)
 
